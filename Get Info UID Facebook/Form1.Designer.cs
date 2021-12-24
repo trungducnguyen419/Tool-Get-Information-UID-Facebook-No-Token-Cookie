954bea326d781a811e3afab699ebc27ef498f273
Nguyễn Trung Đức <54103645+trungducnguyen419@users.noreply.github.com>
2021-12-24 19:53:15 +0000

    Add files via upload

---
+++ Get Info UID Facebook/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace Get_Info_UID_Facebook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(285, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Info";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(262, 153);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 14);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "List UID:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column10,
+            this.Column8,
+            this.Column9});
+            this.dataGridView1.Location = new System.Drawing.Point(17, 186);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(837, 157);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "#";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 50;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Id";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Name";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Avatar";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 70;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Birthday";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Email";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Phone";
+            this.Column7.Name = "Column7";
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "Follow";
+            this.Column10.Name = "Column10";
+            this.Column10.Width = 70;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Friend";
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 70;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Created Date";
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 150;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(285, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 25);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Save Info";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(866, 355);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Get Info UID Facebook";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Get Info UID Facebook/Form1.cs
@@ -0,0 +1,231 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Get_Info_UID_Facebook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+            Button.CheckForIllegalCrossThreadCalls = false;
+            DataGridView.CheckForIllegalCrossThreadCalls = false;
+        }
+        bool isStop = false;
+        public struct InfoUID
+        {
+            public string UrlAvatar { get; set; }
+            public string Name { get; set; }
+            public string Birthday { get; set; }
+            public string Email { get; set; }
+            public string Phone { get; set; }
+            public int Friend { get; set; }
+            public int Follow { get; set; }
+            public string CreatedTime { get; set; }
+        }
+        public class GraphQLFacebook
+        {
+            private string uid { get; set; }
+            private HttpClient httpClient = new HttpClient();
+            public InfoUID ResultInfo = new InfoUID();
+            public GraphQLFacebook(string uid)
+            {
+                this.uid = uid;
+                httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"); //thêm user agent để lấy đc danh sách ảnh
+                httpClient.DefaultRequestHeaders.Add("Cookie", "locale=vi_VN"); //đặt ngôn ngữ facebook thành ngôn ngữ Việt Nam
+                httpClient.DefaultRequestHeaders.Add("sec-fetch-site", "same-origin"); //thêm sec-fetch-site để lấy đc danh sách ảnh
+            }
+            public async Task GetUrlAvatar()
+            {
+                var content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("doc_id", "5341536295888250"),
+                    new KeyValuePair<string, string>("variables", "{\"height\":500,\"scale\":1,\"userID\":\"" + uid + "\",\"width\":500}")
+                });
+                var response = await httpClient.PostAsync("https://www.facebook.com/api/graphql/", content);
+                string result = response.Content.ReadAsStringAsync().Result;
+                dynamic json = JsonConvert.DeserializeObject(result); //parse json
+                if (json.data.profile != null && json.data.profile.profile_picture != null)
+                {
+                    ResultInfo.UrlAvatar = (string)json.data.profile.profile_picture.uri;
+                }
+            }
+            public async Task GetName()
+            {
+                var content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("q", "node(" + uid + "){name}")
+                });
+                var response = await httpClient.PostAsync("https://www.facebook.com/api/graphql/", content);
+                string result = response.Content.ReadAsStringAsync().Result;
+                dynamic json = JsonConvert.DeserializeObject(result.Replace(uid, "info")); //parse json
+                if (json.info != null && json.info.name != null)
+                {
+                    ResultInfo.Name = (string)json.info.name;
+                }
+            }
+            public async Task GetBirthday()
+            {
+                var content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("q", "node(" + uid + "){birthday}")
+                });
+                var response = await httpClient.PostAsync("https://www.facebook.com/api/graphql/", content);
+                string result = response.Content.ReadAsStringAsync().Result;
+                dynamic json = JsonConvert.DeserializeObject(result.Replace(uid, "info")); //parse json
+                if (json.info != null && json.info.birthday != null)
+                {
+                    ResultInfo.Birthday = (string)json.info.birthday;
+                }
+            }
+            public async Task GetEmail()
+            {
+                var content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("q", "node(" + uid + "){email}")
+                });
+                var response = await httpClient.PostAsync("https://www.facebook.com/api/graphql/", content);
+                string result = response.Content.ReadAsStringAsync().Result;
+                dynamic json = JsonConvert.DeserializeObject(result.Replace(uid, "info")); //parse json
+                if (json.info != null && json.info.email != null)
+                {
+                    ResultInfo.Email = (string)json.info.email;
+                }
+            }
+            public async Task GetPhone()
+            {
+                var content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("q", "node(" + uid + "){phone}")
+                });
+                var response = await httpClient.PostAsync("https://www.facebook.com/api/graphql/", content);
+                string result = response.Content.ReadAsStringAsync().Result;
+                dynamic json = JsonConvert.DeserializeObject(result.Replace(uid, "info")); //parse json
+                if (json.info != null && json.info.phone != null)
+                {
+                    ResultInfo.Phone = (string)json.info.phone;
+                }
+            }
+            public async Task GetFriend()
+            {
+                var content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("q", "node(" + uid + "){friends{count}}")
+                });
+                var response = await httpClient.PostAsync("https://www.facebook.com/api/graphql/", content);
+                string result = response.Content.ReadAsStringAsync().Result;
+                dynamic json = JsonConvert.DeserializeObject(result.Replace(uid, "info")); //parse json
+                if (json.info != null && json.info.friends != null)
+                {
+                    ResultInfo.Friend = (int)json.info.friends.count;
+                }
+            }
+            public async Task GetFollow()
+            {
+                var content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("q", "node(" + uid + "){subscribers{count}}")
+                });
+                var response = await httpClient.PostAsync("https://www.facebook.com/api/graphql/", content);
+                string result = response.Content.ReadAsStringAsync().Result;
+                dynamic json = JsonConvert.DeserializeObject(result.Replace(uid, "info")); //parse json
+                if (json.info != null && json.info.subscribers != null)
+                {
+                    ResultInfo.Follow = (int)json.info.subscribers.count;
+                }
+            }
+            public async Task GetCreatedTime()
+            {
+                var content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("q", "node(" + uid + "){created_time}")
+                });
+                var response = await httpClient.PostAsync("https://www.facebook.com/api/graphql/", content);
+                string result = response.Content.ReadAsStringAsync().Result;
+                dynamic json = JsonConvert.DeserializeObject(result.Replace(uid, "info")); //parse json
+                if (json.info != null && json.info.created_time != null)
+                {
+                    ResultInfo.CreatedTime = DateTimeOffset.FromUnixTimeSeconds((int)json.info.created_time).DateTime.ToString();
+                }
+            }
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Get Info")
+            {
+                button1.Text = "Stop";
+                isStop = false;
+                await Task.Run(async () =>
+                {
+                    string[] uids = textBox1.Lines;
+                    for (var i = 0; i < uids.Length; i++)
+                    {
+                        if (isStop) return;
+                        string uid = uids[i];
+                        if (!string.IsNullOrEmpty(uid))
+                        {
+                            var fb = new GraphQLFacebook(uid);
+                            await fb.GetName();
+                            await fb.GetBirthday();
+                            await fb.GetEmail();
+                            await fb.GetPhone();
+                            await fb.GetFollow();
+                            await fb.GetFriend();
+                            await fb.GetCreatedTime();
+                            await fb.GetUrlAvatar();
+                            int add = dataGridView1.Rows.Add();
+                            DataGridViewRow row = dataGridView1.Rows[add];
+                            row.Cells[0].Value = (row.Index + 1).ToString();
+                            row.Cells[1].Value = uid;
+                            row.Cells[2].Value = fb.ResultInfo.Name;
+                            row.Cells[3].Value = fb.ResultInfo.UrlAvatar;
+                            row.Cells[4].Value = fb.ResultInfo.Birthday;
+                            row.Cells[5].Value = fb.ResultInfo.Email;
+                            row.Cells[6].Value = fb.ResultInfo.Phone;
+                            row.Cells[7].Value = fb.ResultInfo.Follow;
+                            row.Cells[8].Value = fb.ResultInfo.Friend;
+                            row.Cells[9].Value = fb.ResultInfo.CreatedTime;
+                        }    
+                    }
+                    button1.Text = "Get Info";
+                });
+            }    
+            else
+            {
+                button1.Text = "Get Info";
+                isStop = true;
+            }   
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string info = "";
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    string data = "";
+                    for (var i = 1; i < dataGridView1.Columns.Count; i++)
+                    {
+                        data += row.Cells[i].Value?.ToString() + "|";
+                    }
+                    data = data.Substring(0, data.Length - 1);
+                    info += data + "\r\n";
+                }
+                File.WriteAllText(saveFileDialog.FileName.ToString(), info);
+            }    
+        }
+    }
+}
